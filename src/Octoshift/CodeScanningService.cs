c67e10890ebd3deddebf57598b4c8754268f1b64
David Losert <davelosert@github.com>
2022-08-19 12:12:37 +0200

    Adds more logging and exception handling to CodeScanningService

--- src/Octoshift/CodeScanningService.cs
+++ src/Octoshift/CodeScanningService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Octoshift.Models;
@@ -21,23 +22,35 @@ namespace Octoshift
         public virtual async Task MigrateAnalyses(string sourceOrg, string sourceRepo, string targetOrg,
             string targetRepo)
         {
-            _log.LogInformation($"Migrating Code Scanning Analyses from '{sourceOrg}/${sourceRepo}' ${targetOrg}/${targetRepo}");
+            _log.LogInformation($"Migrating Code Scanning Analyses from '{sourceOrg}/{sourceRepo}' to '{targetOrg}/{targetRepo}'");
             var analyses = await _sourceGithubApi.GetCodeScanningAnalysisForRepository(sourceOrg, sourceRepo);
+            var count = 0;
+            var errorCount = 0;
             
             analyses = analyses.OrderBy(a => a.CreatedAt).ToList();
+            _log.LogVerbose($"Found {analyses.Count()} analyses to migrate.");
             
             foreach (var analysis in analyses)
             {
                 var sarifReport = await _sourceGithubApi.GetSarifReport(sourceOrg, sourceRepo, analysis.Id);
-                await _targetGithubApi.UploadSarifReport(targetOrg, targetRepo, new SarifContainer
+                _log.LogVerbose($"Downloaded SARIF report for analysis {analysis.Id}");
+                try
                 {
-                    sarif = sarifReport,
-                    Ref = analysis.Ref,
-                    CommitSha = analysis.CommitSha
-                });
+                    await _targetGithubApi.UploadSarifReport(targetOrg, targetRepo,
+                        new SarifContainer { sarif = sarifReport, Ref = analysis.Ref, CommitSha = analysis.CommitSha });
+                    count++;
+                    _log.LogInformation($"Successfully Migrated report for analysis {analysis.Id}");
+                }
+                catch (Exception exception)
+                {
+                    _log.LogWarning($"Error while uploading SARIF report for analysis {analysis.Id}: \n {exception.Message}");
+                    _log.LogError(exception);
+                    errorCount++;
+                }
+                
+                _log.LogInformation($"Code Scanning Analyses done!\nSuccess-Count: {count}\nError-Count: {errorCount}\nOverall: {analyses.Count()}.");
             }
         }
-        
     }
 
 }
